9e22adfa1cb50a24631f7911e305038c872e43ce
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-20 23:56:09 +0900

    Create custom-form-button.js

---
+++ assets/javascripts/discourse/initializers/custom-form-button.js
@@ -0,0 +1,32 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import CustomFormModal from "../components/custom-form-modal";
+
+function initializeCustomFormButton(api) {
+  api.onToolbarCreate((toolbar) => {
+    toolbar.addButton({
+      id: "custom_form",
+      group: "extras",
+      icon: "plus",
+      title: "custom_form.button_title",
+      perform: (editor) => {
+        const modal = editor.container.lookup("service:modal");
+        modal.show(CustomFormModal, {
+          model: {
+            editor: editor
+          }
+        });
+      }
+    });
+  });
+}
+
+export default {
+  name: "custom-form-button",
+  initialize(container) {
+    const siteSettings = container.lookup("service:site-settings");
+    
+    if (siteSettings.custom_form_enabled) {
+      withPluginApi("0.12.0", initializeCustomFormButton);
+    }
+  }
+};
